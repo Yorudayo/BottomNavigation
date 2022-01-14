f3dcb58efc8e3f60f3532693d363094bc9a1c7d2
Yorudayo <83815054+Yorudayo@users.noreply.github.com>
2022-01-14 17:14:00 +0900

    0114

--- app/src/main/java/android/lifeistech/bottomnavigation/MainActivity.kt
+++ app/src/main/java/android/lifeistech/bottomnavigation/MainActivity.kt
@@ -2,11 +2,17 @@ package android.lifeistech.bottomnavigation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navController = findNavController(R.id.nav_host_fragment)
+        setupWithNavController(bottom_navigation, navController)
     }
 }
---
+++ app/src/main/java/android/lifeistech/bottomnavigation/fragment_break.kt
@@ -0,0 +1,14 @@
+package android.lifeistech.bottomnavigation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class fragment_break : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_break, container, false)
+    }
+}
