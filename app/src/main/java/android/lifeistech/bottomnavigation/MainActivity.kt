fd95662fc0083f5d2df120ff99cd7b1a5c9d041b
Yorudayo <83815054+Yorudayo@users.noreply.github.com>
2022-02-18 20:24:44 +0900

    0218②

--- app/src/main/java/android/lifeistech/bottomnavigation/MainActivity.kt
+++ app/src/main/java/android/lifeistech/bottomnavigation/MainActivity.kt
@@ -9,9 +9,7 @@ import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
--- app/src/main/java/android/lifeistech/bottomnavigation/WriteActivity.kt
+++ app/src/main/java/android/lifeistech/bottomnavigation/WriteActivity.kt
@@ -1,18 +1,43 @@
 package android.lifeistech.bottomnavigation
 
 import android.content.Intent
+import android.lifeistech.bottomnavigation.databinding.ActivityMainBinding
+import android.lifeistech.bottomnavigation.databinding.ActivityWriteBinding
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_write.*
 
 class WriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityWriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_write)
+        binding = ActivityWriteBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_write)
+        setContentView(binding.root)
 
+        val db = Firebase.firestore
         saveButton.setOnClickListener{
+            val task = Task(
+                title = binding.titleEditText.text.toString(),
+                summarytext = binding.summaryEditText.toString(),
+            )
+
+            db.collection("tasks")
+                .add(task)
+                .addOnSuccessListener { documentReference ->
+                    Log.d(ADD_TAG,"DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener{ e ->
+                    Log.w(ADD_TAG, "Error adding document", e)
+                }
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
         }
+    }
+    companion object {
+        private const val ADD_TAG = "add_task"
     }
 }
